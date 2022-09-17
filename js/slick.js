e5fc6b9d33b1ad934a0c1ec181fa6daf6a14acb1
@MariamMandour <m.mandour@hotdesk.io>
2022-09-18 00:21:00 +0200

    testmonial commit

--- js/slick.js
+++ js/slick.js
@@ -46,4 +46,53 @@ $(document).ready(function () {
     setInterval(function () {
         $(".slider").slick("slickNext");
     }, 5000);
+    // ------------------------------------------------------------------------------------------------------
+
+    $(".slider_2").slick({
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        accessibility: false,
+        initialSlide: 1,
+        fade: true,
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    accessibility: false,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    accessibility: false,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    accessibility: false,
+                    slidesToScroll: 1,
+                    infinite: true,
+                },
+            },
+        ],
+    });
+    $(".next_2").click(function () {
+        $(".slider_2").slick("slickNext");
+    });
+    $(".prev_2").click(function () {
+        $(".slider_2").slick("slickPrev");
+    });
+    setInterval(function () {
+        $(".slider_2").slick("slickNext");
+    }, 5000);
 });
